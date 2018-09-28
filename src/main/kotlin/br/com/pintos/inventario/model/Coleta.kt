03ec9b0cc76fd4df3fce0af56c63380e763b5b3d
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2018-09-28 15:32:34 -0300

    Imprementação de funções na classe Coleta

--- src/main/kotlin/br/com/pintos/inventario/model/Coleta.kt
+++ src/main/kotlin/br/com/pintos/inventario/model/Coleta.kt
@@ -1,6 +1,7 @@
 package br.com.pintos.inventario.model
 
 import br.com.pintos.framework.model.BaseModel
+import br.com.pintos.inventario.model.EStatusColeta.ABERTO
 import br.com.pintos.inventario.model.finder.ColetaFinder
 import javax.persistence.Entity
 import javax.persistence.EnumType
@@ -12,16 +13,17 @@ import javax.persistence.Table
 @Entity
 @Table(name = "coleta")
 class Coleta(
-        var numleitura: Int,
-        @ManyToOne
-        @JoinColumn(name = "Inventario_id")
-        var inventario: Inventario,
-        @ManyToOne
-        var lote: Lote,
-        @ManyToOne
-        var usuario: Usuario, var coletor: Int,
-        @Enumerated(EnumType.STRING)
-        var status: EStatusColeta
+  var numleitura: Int,
+  @ManyToOne
+  @JoinColumn(name = "Inventario_id")
+  var inventario: Inventario,
+  @ManyToOne
+  var lote: Lote,
+  @ManyToOne
+  var usuario: Usuario,
+  var coletor: Int,
+  @Enumerated(EnumType.STRING)
+  var status: EStatusColeta
 ) : BaseModel() {
   fun quantidadeLeitura(): Int {
     return Leitura.where()
@@ -30,9 +32,31 @@ class Coleta(
             .findCount()
   }
 
-  companion object Find : ColetaFinder()
+  companion object Find : ColetaFinder() {
+    fun findColetaAberta(inventario: Inventario?, lote: Lote?, usuario: Usuario?): Coleta? {
+      inventario ?: return null
+      lote ?: return null
+      usuario ?: return null
+      return where().inventario.id.eq(inventario.id)
+              .lote.id.eq(lote.id)
+              .usuario.id.eq(usuario.id)
+              .status.eq(ABERTO)
+              .orderBy().numleitura.asc()
+              .findList()
+              .firstOrNull()
+    }
+
+    fun novoNumero(inventario: Inventario, lote: Lote): Int {
+      return (where().inventario.id.eq(inventario.id)
+              .lote.id.eq(lote.id)
+              .findList()
+              .asSequence()
+              .map { it.numleitura }
+              .max() ?: 0) + 1
+    }
+  }
 }
 
 enum class EStatusColeta {
-    FECHADO, ABERTO
+  FECHADO, ABERTO
 }
