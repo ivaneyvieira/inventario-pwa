7316b2e408e9cf6aa420fd5175326ff56bae81d2
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-03-24 16:53:04 -0300

    Atualização

--- src/main/kotlin/br/com/pintos/framework/viewmodel/SubViewModel.kt
+++
@@ -1,9 +0,0 @@
-package br.com.pintos.framework.viewmodel
-
-import kotlin.reflect.KClass
-
-open class SubViewModel(view: IView, private val classView : KClass<*>) : ViewModel(view) {
-  fun doVoltar() = exec {
-    view.navigate(classView)
-  }
-}
